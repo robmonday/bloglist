31d48fd7340641d55e3b33f011b821b3c7ba9343
“robmonday” <10903531+robmonday@users.noreply.github.com>
2022-12-14 15:55:14 -0500

    exercise 4.15

--- controllers/bloglist.js
+++ controllers/bloglist.js
@@ -1,23 +1,28 @@
 const bloglistRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 
-bloglistRouter.get('/', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+bloglistRouter.get('/', async (request, response) => {
+  const bloglist = await Blog.find({})
+  response.json(bloglist)
 })
 
-bloglistRouter.post('/', (request, response) => {
-  const blog = new Blog(request.body)
+bloglistRouter.post('/', async (request, response) => {
+  const body = request.body
+  const user = await User.findById(body.userId)
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+  const blog = new Blog({
+    title: body.title, 
+    author: body.author, 
+    url: body.url, 
+    user: user._id
+  })
+
+
+  const savedBlog = await blog.save()
+  
+  response.status(201).json(savedBlog)
 })
 
 module.exports = bloglistRouter
--- models/blog.js
+++ models/blog.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose')
 
 const blogSchema = new mongoose.Schema({
-  title: String,
+  title: { type: String, required: true, minlength: 5 },
   author: String,
-  url: String,
-  likes: Number 
+  url: { type: String, required: true, minlength: 5 },
+  likes: Number, 
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  } 
 })
 
 blogSchema.set('toJSON', {
