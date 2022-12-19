7e74617cfe9c96dc13a191278c4e77b77c6a678a
“robmonday” <10903531+robmonday@users.noreply.github.com>
2022-12-19 16:48:48 -0500

    exercise 4.18 completed

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ const app = express()
 const cors = require('cors')
 const bloglistRouter = require('./controllers/bloglist')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
 logger.info('connecting to', config.MONGODB_URI)
 
-mongoose.connect(config.MONGODB_URI)  // is this the right parameter?
+mongoose.connect(config.MONGODB_URI)
   .then(() => 
     logger.info('connected to MongoDB'))
   .catch(error => 
@@ -22,6 +23,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/bloglist', bloglistRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
